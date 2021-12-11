18b476430e412681d51d8f1030953c1245e842e9
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-12-11 23:59:40 +0300

    Added products entity class and repository interface

--- src/main/java/com/kamilifu/ms_hardware_products/dblayer/entities/Products.java
+++ src/main/java/com/kamilifu/ms_hardware_products/dblayer/entities/Products.java
@@ -5,10 +5,43 @@
  */
 package com.kamilifu.ms_hardware_products.dblayer.entities;
 
+import com.kamilifu.ms_hardware_products.dblayer.entities.enums.Units;
+import java.util.Date;
+import javax.persistence.*;
+import lombok.*;
 /**
  *
  * @author kmakamu
  */
+@Entity
+@Table(name="products")
+@Getter
+@Setter
+@ToString
 public class Products {
+  @Id
+  @Setter(AccessLevel.PROTECTED)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+  @Column(name="name",length=75,nullable=false,unique=true)
+  private String name;
   
+  @Column(name="restockLevel")
+  private int restockLevel;
+  
+  @Column(name="inStock")
+  private int inStock;
+  
+  @Enumerated(EnumType.STRING)
+  private Units units;
+  
+  @Column(name="isActive")
+  private boolean isActive;
+  
+  @Column(name="createdAt")
+  private Date createdAt;
+  
+  @Column(name="updatedAt")
+  private Date updatedAt;
 }
---
+++ src/main/java/com/kamilifu/ms_hardware_products/dblayer/repositories/ProductRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kamilifu.ms_hardware_products.dblayer.repositories;
+
+import com.kamilifu.ms_hardware_products.dblayer.entities.Products;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author kmakamu
+ */
+@Repository
+public interface ProductRepository extends CrudRepository<Products,Long>{
+  
+}
