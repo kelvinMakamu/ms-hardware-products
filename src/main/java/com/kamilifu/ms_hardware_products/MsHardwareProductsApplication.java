94e1bad405417667e2e6a8fc09d3d6462e8e4298
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-12-11 20:54:16 +0300

    Added application and datasource properties

---
+++ src/main/java/com/kamilifu/ms_hardware_products/MsHardwareProductsApplication.java
@@ -0,0 +1,13 @@
+package com.kamilifu.ms_hardware_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHardwareProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHardwareProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamilifu/ms_hardware_products/MsHardwareProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilifu.ms_hardware_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHardwareProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
